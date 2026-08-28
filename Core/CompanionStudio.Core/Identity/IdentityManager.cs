0faffcaa6d629d27f8152d7f936fe26b075e38e3
CHECO <checo@local>
2026-08-28 04:31:55 -0600

    Implement IdentityManager

--- Core/CompanionStudio.Core/Identity/IdentityManager.cs
+++ Core/CompanionStudio.Core/Identity/IdentityManager.cs
@@ -1,7 +1,25 @@
-﻿namespace CompanionStudio.Core.Identity
-{
-    public class IdentityManager
-    {
+﻿namespace CompanionStudio.Core.Identity;
 
+public class IdentityManager
+{
+    private readonly List<IdentityModel> identities = new();
+
+    public IdentityModel CreateIdentity(string name, string description)
+    {
+        var identity = new IdentityModel
+        {
+            Id = $"CS-{identities.Count + 1:000000}",
+            Name = name,
+            Description = description
+        };
+
+        identities.Add(identity);
+
+        return identity;
+    }
+
+    public IEnumerable<IdentityModel> GetIdentities()
+    {
+        return identities;
     }
 }
