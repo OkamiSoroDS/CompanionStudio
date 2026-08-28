09fd3582e0f4b3184acd793d4aebd768ff161847
CHECO <checo@local>
2026-08-28 05:07:57 -0600

    Add Identity service layer

---
+++ Core/CompanionStudio.Core/Services/IdentityService.cs
@@ -0,0 +1,22 @@
+﻿using CompanionStudio.Core.Identity;
+
+namespace CompanionStudio.Core.Services;
+
+public class IdentityService
+{
+    private readonly IdentityManager manager;
+
+    public IdentityService()
+    {
+        manager = new IdentityManager();
+    }
+
+    public IdentityModel Create(
+        string name,
+        string description)
+    {
+        return manager.CreateIdentity(
+            name,
+            description);
+    }
+}
